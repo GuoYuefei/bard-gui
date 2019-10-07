4bc25a5cd7034a34ec890a5db6a9b2f58ce94cae
Solomon Guo <beconegood@gmail.com>
2019-10-07 23:10:26 +0800

    添加windows后台运行依赖

--- bard/install-bard.cmd
+++ bard/install-bard.cmd
@@ -28,7 +28,7 @@ goto:eof
 
 :compile
 	:: 编译主程序
-	go build -o bard-client client/client.go
+	go build -o bard-client.exe client/client.go
 	move /y bard-client ..
 	:: cp -rf client ..
 	:: shell同上作用
