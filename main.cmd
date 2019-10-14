b808c029e6909e4995b93d315d27428f065821ea
Solomon Guo <beconegood@gmail.com>
2019-10-14 21:27:32 +0800

    cmd

--- main.cmd
+++ main.cmd
@@ -1,1 +1,8 @@
-cmd /c go build cmd/main.go && start bard/hideexec.exe main.exe
+:: cmd /c go build cmd/main.go && start bard/hideexec.exe main.exe
+
+cmd /c
+if not exist main.exe (
+    go build cmd/main.go
+)
+
+start bard/hideexec.exe main.exe
