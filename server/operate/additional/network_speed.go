bfb84cd722ad14cdfbe6777a2624af44ae5ce31e
GuoYuefei <beconegood@gmail.com>
2019-10-22 21:25:46 +0800

    icmp 校验和算法， 通过单元测试

--- server/operate/additional/network_speed.go
+++ server/operate/additional/network_speed.go
@@ -37,3 +37,60 @@ func NetworkSpend() (string, error) {
 
 	return string(output), nil
 }
+
+type ICMP_Head struct {
+	Type uint8
+	Code uint8
+	CheckSum []byte
+	Identifier []byte
+	SeqNum []byte
+	Data []byte
+}
+
+func NewICMP_Head(t uint8, code uint8, ide uint16, seq uint16) *ICMP_Head {
+
+	return &ICMP_Head{
+		Type:       t,
+		Code:       code,
+		CheckSum:   []byte{0, 0},
+		Identifier: []byte{byte(ide>>8), byte(ide)},
+		SeqNum:     []byte{byte(seq>>8), byte(seq)},
+		Data:       []byte{},
+	}
+}
+
+// icmp echo
+// type 8 code 0
+// icmp reply
+// type 0 code 0
+
+
+// 正常加法 但是溢出一次自动+1一次
+func (i *ICMP_Head) DoCheckSum() uint16 {
+	var sum uint16 = 0
+	Data := []byte{
+		i.Type, i.Code, 0x00, 0x00,
+		i.Identifier[0], i.Identifier[1],
+		i.SeqNum[0], i.SeqNum[1],
+	}
+	Data = append(Data, i.Data...)
+	if len(Data) % 2 != 0 {
+		Data = append(Data, 0)
+	}
+	for j := 0; j < len(Data)/2; j++ {
+		temp := sum
+		sum += uint16(Data[2*j])<<8 + uint16(Data[2*j+1])
+		if sum < temp {				// 不能== 因为可能存在0
+			// 溢出了
+			sum++
+		}
+		//fmt.Printf("%d:\t sum is %x\n", j, sum)
+	}
+	return sum
+}
+
+
+
+func GoPing() {
+
+}
--- server/operate/additional/network_speed_test.go
+++ server/operate/additional/network_speed_test.go
@@ -1,10 +1,37 @@
 package additional
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestNetworkSpend(t *testing.T) {
 	t.Log(NetworkSpend())
 
+}
+
+func TestICMP_Head_DoCheckSum(t *testing.T) {
+	icmp := NewICMP_Head(8, 0, 1, 1)
+	icmp.Data = []byte{
+
+		// type=0 code=0, ide=0, seq=0,     // will result should be 4e19, The inverse code is B1E6
+		//0x45, 0x00, 0x00, 0x3c,
+		//0x1c, 0x46,
+		//0x40, 0x00,
+		//0x40, 0x06, 0x00, 0x00,
+		//0xac, 0x10, 0x0a, 0x63,
+		//0xac, 0x10, 0x0a, 0x0c,
+
+		// type=8, code=0, ide=1, seq=1,     // will result should be b2a5, The inverse code is 4d5a
+		0X61, 0x62, 0X63, 0x64,
+		0X65, 0x66, 0X67, 0x68,
+		0X69, 0x6a, 0X6b, 0x6c,
+		0X6d, 0x6e, 0X6f, 0x70,
+		0X71, 0x72, 0X73, 0x74,
+		0X75, 0x76, 0X77, 0x61,
+		0X62, 0x63, 0X64, 0x65,
+		0X66, 0x67, 0X68, 0x69,
+	}
+	sum := icmp.DoCheckSum()
+	fmt.Printf("sum 16进制：%x,\nsum 10进制: %d,\nsum 反码16进制: %x\n", sum, sum, ^sum)
 }
